7b34caebcd3c03e34610056e2f6551c1b48f42f7
Martin Wilz <github@wilz.de>
2014-05-22 10:29:34 +0200

    first implementation of bar button item subclass, added pod file for example & added bar buttons to storyboard for testing it out

---
+++ Classes/MWAwesomeFontBarButtonItem.h
@@ -0,0 +1,13 @@
+//
+//  MWAwesomeFontBarButtonItem.h
+//  MWAwesomeFontWrapperExample
+//
+//  Created by martin on 22/05/14.
+//  Copyright (c) 2014 Martin Wilz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MWAwesomeFontBarButtonItem : UIBarButtonItem
+
+@end
